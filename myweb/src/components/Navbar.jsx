8570ea4794ed71c4eacc994bd1d9f96d21a2f01a
ZANC0 <zanco.farrell@gmail.com>
2024-12-26 23:48:05 +0000

    Incorporating static blog
    Because its my personal website doesnt serve anyone a service, its simply a place of learning more about me.

--- myweb/src/components/Navbar.jsx
+++ myweb/src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom';
 import React from 'react';
 import "./Navbar.css"
 import AboutMe from '../pages/AboutMe';
+import Blog from '../pages/Blog';
 function Navbar(){
     return(
         <>
             <div className='navbar'>
                 <Link to="/">about me</Link>            
                 <Link to="/projects">projects</Link>
+                <Link to="/blog">blog</Link>
             </div>
         </>
     )
--- myweb/src/pages/Blog.jsx
+++ myweb/src/pages/Blog.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react"
+import { createRequestHandler } from "react-router-dom"
+import posts from "../posts.json"
 function Blog(){
+    const [showdata, setdata] = useState(posts)
+    const logdata = () => {
+        console.log(showdata)
+    }
+
     return(
         <>
             <div>
                 <h1>Blog</h1>
+                {
+                    showdata.posts.map((item, index)=>{
+                        return(
+                            <div key={index}>
+                                <p>{item.title}</p>
+                                <p>{item.date}</p>
+                                <p>{item.content}</p>
+                            </div>
+                        )
+                        
+                    })
+                }
             </div>
         </>
     )
